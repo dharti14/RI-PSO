cf25252d94f4d1b5cd0359e9052868e3c113daea
DipeshRegur <dipesh@regur.net>
2016-01-12 20:03:46 +0530

    Changes in css according to view in mobile

--- wp-content/themes/genesis-removals-index/lp1/lib/structure/header.php
+++ wp-content/themes/genesis-removals-index/lp1/lib/structure/header.php
@@ -31,6 +31,11 @@ if($hln == 'Trusted Local Removal Companies'){
 }
 ?></title>
 
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+  <script src="js/html5shiv.min.js"></script>
+  <script src="js/respond.min.js"></script>
+<![endif]-->
 
 <?php 
 
