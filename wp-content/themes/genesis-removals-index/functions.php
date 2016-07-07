0ced30142c7a80b4ce1b1ce03da969e63b27ce75
ravi m <ravi@regur.net>
2016-07-07 12:49:05 +0530

    Fixed redirect for '/2thanks' page

--- wp-content/themes/genesis-removals-index/functions.php
+++ wp-content/themes/genesis-removals-index/functions.php
@@ -115,9 +115,10 @@ add_filter('rewrite_rules_array', 'ri_rewrite_rules' , 1 , 1);
 
 
 function ri_page_template_redirect(){
+	
 	$req_uri = $_SERVER['REQUEST_URI'];
 	$req_uri_array = explode('?', $req_uri);
-    if( $req_uri_array[0] == '/2thanks/' ) {
+    if( $req_uri_array[0] == '/2thanks/' || $req_uri_array[0] == '/2thanks' ) {
     	wp_redirect(home_url().'/2/thanks/');
         exit();
     }
