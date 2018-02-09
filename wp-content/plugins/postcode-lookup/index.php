5637e9b60070a709f6cea0b9ab21c066bc110f2b
Dharti Mehta <dharti@regur.net>
2018-02-09 13:32:14 +0530

    Updated required code and added new field in pl plugin

--- wp-content/plugins/postcode-lookup/index.php
+++ wp-content/plugins/postcode-lookup/index.php
@@ -10,8 +10,7 @@
 
 class PostcodeLookup {
 	public function __construct() {
-		add_action('wp_enqueue_scripts', array($this,'postcode_lookup_scripts' ));
-		add_action('admin_enqueue_scripts', array($this,'postcode_lookup_admin_scripts' ));
+		add_action('wp_enqueue_scripts', array($this,'postcode_lookup_scripts' ));		
 		add_action('admin_menu', array($this,'postcode_lookup_add_page'));
 		add_action('admin_post_postcode_lookup_option', array($this,'postcode_lookup_options' ));
 		
@@ -39,7 +38,7 @@ class PostcodeLookup {
 							<label for="postcode_lookup_api_url">API URL</label>
 						</th>
 						<td>
-							<input type="text" class="pl-width" name="postcode_lookup_api_url" value="<?php echo esc_html( $options['postcode_lookup_url'] ); ?>"/>
+							<input type="text" class="regular-text" name="postcode_lookup_api_url" value="<?php if(isset($options['postcode_lookup_api_url'])){ echo esc_html( $options['postcode_lookup_api_url'] ); } ?>"/>
 						</td>
 					</tr>
 					<tr class="form-required">
@@ -47,15 +46,22 @@ class PostcodeLookup {
 							<label for="postcode_lookup_api_key">API Key</label>
 						</th>
 						<td>
-							<input type="text"  class="pl-width" name="postcode_lookup_api_key" value="<?php echo esc_html( $options['postcode_lookup_key'] ); ?>"/>
+							<input type="text"  class="regular-text" name="postcode_lookup_api_key" value="<?php if(isset($options['postcode_lookup_api_key'])){ echo esc_html( $options['postcode_lookup_api_key'] ); } ?>"/>
 						</td>
 					</tr>
-					<tr>
-					 <td> <input type="submit" value="Submit" class="button-primary"/></td>
-					</tr>
-					
+					<tr class="form-required">
+						<th scope="row">
+							<label for="postcode_lookup_plugin_selector">Plugin Intialization Selector</label>
+						</th>
+						<td>
+							<input type="text"  class="regular-text" name="postcode_lookup_plugin_init_selector" value="<?php if(isset($options['postcode_lookup_plugin_init_selector'])){ echo esc_html( $options['postcode_lookup_plugin_init_selector'] ); } ?>"/>
+						</td>
+					</tr>				
 				</tbody>
 			</table>
+			<div class="submit">
+				<input type="submit" value="Submit" class="button-primary"/>
+			</div>			
 	     </form>
 	   
 	<?php
@@ -73,33 +79,52 @@ class PostcodeLookup {
 	
 	   $options = get_option( 'postcode_lookup_array' );
 	   
+	  	
+	   if ( isset( $_POST['postcode_lookup_api_url'] ))
+	   {
+	      $options['postcode_lookup_api_url'] = sanitize_text_field( $_POST['postcode_lookup_api_url'] );
+	   }
+	   if ( isset( $_POST['postcode_lookup_api_key'] ))
+	   {
+	   	  $options['postcode_lookup_api_key'] = sanitize_text_field( $_POST['postcode_lookup_api_key'] );
+	   }
+	   if ( isset( $_POST['postcode_lookup_plugin_init_selector'] ))
+	   {
+	   	$options['postcode_lookup_plugin_init_selector'] = sanitize_text_field( $_POST['postcode_lookup_plugin_init_selector'] );
+	   }
 	  
-	   if ( isset( $_POST['postcode_lookup_api_url'] ) && !empty($_POST['postcode_lookup_api_url'] ) )
-	   {
-	      $options['postcode_lookup_url'] = sanitize_text_field( $_POST['postcode_lookup_api_url'] );
-	   }
-	   if ( isset( $_POST['postcode_lookup_api_key'] ) && !empty($_POST['postcode_lookup_api_key'] ) )
-	   {
-	   	$options['postcode_lookup_key'] = sanitize_text_field( $_POST['postcode_lookup_api_key'] );
-	   }
 	   update_option( 'postcode_lookup_array', $options );
 	
-	   wp_redirect(  admin_url( 'options-general.php?page=postcode_lookup' ) );
+	   wp_redirect(admin_url( 'options-general.php?page=postcode_lookup'));
 	   
 	   exit;
 	}
 	public function postcode_lookup_ajax(){
-		
+		 
+		 $initUrl = '';
 		 $searchedKeyword = $_GET['searchedKeyword'];
 		 // pl means postcode lookup	
-		 $options=get_option( 'postcode_lookup_array' );
-		 $pl_url= $options['postcode_lookup_url'];
-		 if(substr($pl_url, -1) == '/') {
-			$pl_url = substr($pl_url, 0, -1);
-		 }
-		 $pl_key = $options['postcode_lookup_key'];
-		 $initUrl = $pl_url.'/'.$pl_key.'?searchedKeyword='.$searchedKeyword;
+		 $options=get_option( 'postcode_lookup_array');
+		 if(!empty($options))
+		 {	 
+		 	if(isset($options['postcode_lookup_api_url']))
+		 	{
+		 		$pl_url= $options['postcode_lookup_api_url'];
+		 		if(substr($pl_url, -1) == '/') {
+		 			$pl_url = substr($pl_url, 0, -1);
+		 		}
+		 	}	
+		 	if(isset($options['postcode_lookup_api_key']))
+		 	{
+		 		$pl_key = $options['postcode_lookup_api_key'];
+		 	}		 
 			
+		 	if($pl_url != '' && $pl_key != '')
+		 	{	
+			 $initUrl = $pl_url.'/'.$pl_key.'?searchedKeyword='.$searchedKeyword;
+		 	} 
+		 }
+		 			 
 		 $ch = curl_init();
 		 curl_setopt($ch, CURLOPT_URL, $initUrl);
 		 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -107,7 +132,8 @@ class PostcodeLookup {
 		 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 		 $output = trim(curl_exec($ch));
 		 echo $output;
-		 curl_close($ch);  
+		 curl_close($ch); 
+			
 		 wp_die();
 
 	}
@@ -122,10 +148,7 @@ class PostcodeLookup {
 	   	 wp_localize_script('postcode_lookup', 'postcode_lookup', array('ajaxurl' => admin_url('admin-ajax.php'))); //create ajaxurl 
 	   	 
    	}
-	public function postcode_lookup_admin_scripts()
-	{
-		wp_enqueue_style( 'postcode_lookup_admin_style', plugins_url( '/css/admin-style.css', __FILE__ ) );
-	}
+	
 }
 
 new PostcodeLookup();
